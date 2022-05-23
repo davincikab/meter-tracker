03a2fd5db4499aba67eb033f8b090972ffda2a83
davincikab <davidnganganjeri079@gmail.com>
2022-05-23 12:41:21 +0300

    sort gltf loader import path

--- src/components/Map/utils/customLayer.js
+++ src/components/Map/utils/customLayer.js
@@ -1,6 +1,6 @@
 // import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
 import * as THREE from 'three';
-import GLTFLoader from 'three-gltf-loader';
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
 // import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
 
 import mapboxgl from 'mapbox-gl';
